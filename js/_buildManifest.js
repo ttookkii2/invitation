65a033ebaaf307ae95f54d86c809a30c1e7f0fed
ttookkii2 <167043092+ttookkii2@users.noreply.github.com>
2024-04-15 14:41:17 +0900

    Create _buildManifest.js

---
+++ js/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = function(s, c) {
+    return {
+        __rewrites: {
+            beforeFiles: [],
+            afterFiles: [],
+            fallback: []
+        },
+        "/": ["static/chunks/pages/index-143db2e222016881.js"],
+        "/20230916": [s, c, "static/chunks/pages/20230916-4d717947769d3489.js"],
+        "/240113": [s, c, "static/chunks/617-ce16126cc4e99af3.js", "static/chunks/pages/240113-8c589c65035dd40b.js"],
+        "/_error": ["static/chunks/pages/_error-54de1933a164a1ff.js"],
+        sortedPages: ["/", "/20230916", "/240113", "/_app", "/_error"]
+    }
+}("static/chunks/321-eff00064521b7016.js", "static/css/392367c23776cfe1.css"), self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
